8c59b0faffe4e5de6314c527941aaffcc08b68a9
lorenaquint <lorenaquint@gmail.com>
2018-03-21 15:04:43 -0500

    Initial commit

---
+++ Encuentas/Encuentas/SurveyDetailsView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Encuentas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SurveyDetailsView : ContentPage
+	{
+		public SurveyDetailsView ()
+		{
+			InitializeComponent ();
+		}
+
+        public void EquipoFavorito_Clicked(Object sender, EventArgs e)
+        {
+
+        }
+        public void Aceptar_Clicked(Object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Encuentas/Encuentas/SurveysView.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Encuentas
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    using System;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SurveysView : ContentPage
+	{
+		public SurveysView ()
+		{
+			InitializeComponent ();
+		}
+        public async void AddSurvey_Clicked(Object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new SurveyDetailsView());
+        }
+
+    }
+}
